1cd7ac8cbad3757cea7d3b7311507c920ecd74f1
leo-vieiraa <leovieira.devv@gmail.com>
2024-02-04 17:06:28 -0300

    First test and dependency setup

---
+++ app/src/test/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesTest.kt
@@ -0,0 +1,47 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.google.common.truth.Truth.assertThat
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.FakeNoteRepository
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetNotesTest {
+    private lateinit var getNotes: GetNotes
+    private lateinit var fakeRepository: FakeNoteRepository
+
+    @Before
+    fun setUp() {
+        fakeRepository = FakeNoteRepository()
+        getNotes = GetNotes(fakeRepository)
+
+        val notesToInsert = mutableListOf<Note>()
+        ('a'..'z').forEachIndexed { index, c ->
+            notesToInsert.add(
+                Note(
+                    title = c.toString(),
+                    content = c.toString(),
+                    timestamp = index.toLong(),
+                    color = index
+                )
+            )
+        }
+        notesToInsert.shuffle()
+        runBlocking {
+            notesToInsert.forEach { fakeRepository.insertNote(it) }
+        }
+    }
+
+    @Test
+    fun `Order notes by title ascending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Title(OrderType.Ascending)).first()
+
+        for (i in 0..notes.size-2) {
+            assertThat(notes[i].title).isLessThan(notes[i+1].title)
+        }
+    }
+}
